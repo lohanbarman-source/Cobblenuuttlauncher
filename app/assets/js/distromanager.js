8bb541d1cda4a512f6baed14ae5b4116590a76ad
sundsc <sun.machib@gmail.com>
2025-11-22 16:33:40 +0100

    set distro URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager')
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 // exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://cosmo.galaderad98.ovh/cobblenuut/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/lohanbarman-source/Cobblenuuttlauncher/main/distribution.json';
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
